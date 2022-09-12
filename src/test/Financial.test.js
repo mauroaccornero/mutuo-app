cadb04fadaab411e7d38a5f00be575c1f75c9237
Mauro Accornero <mauro.accornero@tetrapak.com>
2022-09-12 09:44:17 +0200

    fix: tests and dependencies
    - update test
    - update dependencies
    - remove yarn

--- src/test/Financial.test.js
+++ src/test/Financial.test.js
@@ -4,7 +4,7 @@ it('rata() should return the correct value', () => {
   const capitale = 90000
   const tasso = 1.85
   const durata = 300
-  expect(rata(capitale, tasso, durata)).toBe(374.9308042253228)
+  expect(rata(capitale, tasso, durata)).toBe(374.9308042253166)
 })
 
 it('convertiTasso() should convert 3% to 0.0025%', () => {
@@ -16,28 +16,37 @@ it('ammortamento() should give the correct ammortamento', () => {
   const tasso = 1
   const durata = 2
 
-  const expectedAmmortamento = [
+  const expectedAmmortamento = [{
+    "capitaleVersato": 49.979175343611836,
+    "date": "Invalid date",
+    "debitoResiduo": 50.020824656388164,
+    "estinzione": 0,
+    "interessiVersati": 0.08333333333333334,
+    "mese": 1,
+    "quotaCapitale": 49.979175343611836,
+    "quotaInteressi": 0.08333333333333334,
+    "rata": 50.06250867694517,
+    "risparmioEstinzione": 0
+  },
     {
-      debitoResiduo: 50.020824656384825,
-      capitaleVersato: 49.979175343615175,
-      interessiVersati: 0.08333333333333334,
-      mese: 1,
-      quotaCapitale: 49.979175343615175,
-      quotaInteressi: 0.08333333333333334,
-      rata: 50.06250867694851,
-    },
-    {
-      debitoResiduo: -1.6697754290362354e-11,
-      capitaleVersato: 100.0000000000167,
-      interessiVersati: 0.1250173538803207,
-      mese: 2,
-      quotaCapitale: 50.02082465640152,
-      quotaInteressi: 0.04168402054698735,
-      rata: 50.06250867694851,
-    },
+      "capitaleVersato": 100.00000000001002,
+      "date": "Invalid date",
+      "debitoResiduo": -1.0018652574217413e-11,
+      "estinzione": 0,
+      "interessiVersati": 0.12501735388032348,
+      "mese": 2,
+      "quotaCapitale": 50.02082465639818,
+      "quotaInteressi": 0.04168402054699014,
+      "rata": 50.06250867694517,
+      "risparmioEstinzione": 0
+    }
   ]
 
-  expect(ammortamento(capitale, tasso, durata)).toEqual(expectedAmmortamento)
+  const today = new Date();
+
+  const estinzioni = [];
+
+  expect(ammortamento({capitale, tasso, durata, today, estinzioni})).toEqual(expectedAmmortamento)
 })
 
 it('interessiRata() should give the correct interess', () => {
